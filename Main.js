327b2719a0ecb87bec33f9f0d3023ce196fea752
Paulo Gustavo Lasalvia Jorge <pglj2@ug1c14.windows.cin.ufpe.br>
2016-10-18 13:35:10 -0300

    Programas teste

--- Main.js
+++ Main.js
@@ -1,8 +1,68 @@
-var a = 1;
-
-function ola (x){
-	var b = 50;
-	var y = x + b;
-	return y;
+//Programa 1
+// Comentario ok? ok
+var a = 0;
+var x = true;
+var k = 100;
+while (x) {
+	if(a==0) {
+	break;
+	a++	
+	}
+break;
 }
-ola(a);
+a; //a = 0 pois o break nao deixou aumentar seu valor
+// while, break e singleif testados!
+
+//Programa 2
+var c;
+var d;
+var e;
+var g;
+var h;
+var i;
+for(i = 0;i<3;i++){
+if(i == 0){
+	g = 1;
+} else {
+	if(i == 1){
+	h = 2;
+	} else {
+		if(i == 2)
+		e = 3;			
+	}
+}
+}
+var f = [g,h,e];
+f; 
+//for testado, if com else testados, e listas.
+
+//Programa 3
+var j = [1,2,3]
+j.head;
+//head testado
+//Programa 4
+var l = [4,5,6,7]
+l.tail;
+//tail testado
+
+//Programa 5
+var ç = l.concat(j);
+ç[7] = l.len;
+ç[8] = j.len;
+ç;
+//len, concat e o setVarArray
+
+//Programa 6
+var u = [];
+var v = [];
+var list1 = [1,2,3];
+var list2 = [1,2,3];
+if (v.equals(u)){
+	var bla = "listas vazias são iguais";
+	if (list1.equals(list2)) {
+	var blabla = "listas com elementos iguals";
+	}
+}
+var teste = [bla,blabla];
+teste;
+
